fb9d7d2cd38ee971bb17b4d80c0dd3c4a96ca1aa
CostaSEVEN <adrianogamer797@gmail.com>
2025-07-30 23:57:51 +0000

    COD

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,15 +27,27 @@ int main() {
 
 printf("Cadrasto carta 1\n");
 
-printf("Digite um estado(letra A a H: ");
+printf("Digite o estado(letra A a H: ");
+scanf(" %c", &estado1);
+
+printf("Digite o código da carta (Ex: A01): ");
+scanf(" %s", codigo1);
+
+printf("Digite o nome da cidade (sem espaços): ");
+scanf(" %s", nomeCidade1);
+
+printf("Digite a população: ");
+scanf("%d", &populacao1);
+
+print("Digite a área em km²: ");
+scnaf("%f", &area1);
 
 
 
 
 
 
-
-
+return 0
 
 
 }
