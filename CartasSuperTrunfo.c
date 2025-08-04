e0034fb9f23335c60c0490c0602b36dece223b11
CostaSEVEN <adrianogamer797@gmail.com>
2025-08-04 23:03:45 +0000

    nova2.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,7 +17,7 @@ int main() {
 
     char estado2;
     char codigo2[4];
-    char comeCidade2[100];
+    char nomeCidade2[100];
     int populacao2;
     float area2;
     float pib2;
@@ -56,10 +56,45 @@ printf("Digite o estado (letra de A a H): ");
 scanf(" %c", &estado2);
 
 printf("Digite o código da carta (Ex: B02): ");
-sscanf("%s", codigo2);
+scanf("%s", codigo2);
 
 printf("Digite o nome da cidade (sem espaço): ");
 scanf("%s", &nomeCidade2);
+
+printf("Digite a população: ");
+scanf("%d", &populacao2);
+
+printf("Digite a área em km²: ");
+scanf("%f", &area2);
+
+printf("Digite o PIB (em bilhões de reais): ");
+scanf("%f", &pib2);
+
+printf("Digite o númerode pontos turísticos: ");
+scanf("%d", &pontosTuristicos2);
+
+//Dados//
+
+printf("\nCarta 1:\n");
+printf("Estado %c\n", estado1);
+printf("Código: %s\n", codigo1);
+printf("Nome da Cidade: %s\n", nomeCidade1);
+printf("População: %d\n", area1);
+printf("Área: %.2f km²\n", area1);
+printf("PIB: %.2f bilhões de reais\n", pib1);
+printf("Número de Pontos Túristicos: %d\n", pontosTuristicos1);
+
+printf("\nCarta 2:\n");
+printf("Estado: %c\n", estado2);
+printf("Código: \n", codigo2);
+printf("Nome da Cidade: %s\n", nomeCidade2);
+printf("População: %d\n", pontosTuristicos2);
+printf("Área: %.2f\n", area2);
+printf("PIB: %.2f km²\n", area2);
+printf("Número de Pontos Túristicos: %d\n", pontosTuristicos2);
+
+return 0;
+
 
 
 
